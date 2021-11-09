55257ac55771b6253fea4399bbeab4a9039b20e5
KrithuRaju <93910945+KrithuRaju@users.noreply.github.com>
2021-11-09 09:08:02 +0530

    Add files via upload

---
+++ SpringSetterBasedInjections/src/com/demo/Message.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+public class Message {
+WriteMessage writeMessage;
+	
+	public WriteMessage getWriteMessage() {
+		return writeMessage;
+	}
+
+	public void setWriteMessage(WriteMessage writeMessage) {
+		this.writeMessage = writeMessage;
+	}
+
+	private String id;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	private String message;
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+	//calling another class mehtod from here
+	public void show() {
+		writeMessage.WriteMessage();
+		
+	}
+
+}
